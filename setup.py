71baa5b71487a0e52a895b1fc2f9dff9ff59dc8d
최민혁 <choiminhyeok8334@gmail.com>
2024-12-31 16:57:54 +0900

    file handler added, create_table added

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 import sqlite3
 
-connection = sqlite3.connect("databse.db")
-cursor = connection.cursor()
+class FileHandler():
+    def __init__(self):
+        self.connection = sqlite3.connect("database.db")
+        self.cursor = self.connection.cursor()
 
+    def create_table(self):
+
+        with open("create_table.sql", "r") as query_file:
+            query_script = query_file.read()
+
+        self.cursor.executescript(query_script)
+        self.connection.commit()
+    
+    def close(self):
+        self.connection.close()
+
+
+file_handler = FileHandler()
+file_handler.create_table()
+file_handler.close()
